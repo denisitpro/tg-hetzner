19fa690698ac3372eea9dc87f484d78462a5fc34
denis.itpro <denis.itpro@gmail.com>
2025-02-25 14:18:15 +0300

    add worker node

--- tf-modules/k8s-htz/cf-k8s-A.tf
+++ tf-modules/k8s-htz/cf-k8s-A.tf
@@ -18,22 +18,22 @@ resource "cloudflare_dns_record" "k8s_master_ipv6" {
   proxied = false
 }
 
-# resource "cloudflare_dns_record" "k8s_worker_ipv4" {
-#   zone_id = local.current_cf_zone_id
-#   count   = length(hcloud_server.k8s_worker)
-#   name    = hcloud_server.k8s_worker[count.index].name
-#   content = hcloud_server.k8s_worker[count.index].ipv4_address
-#   type    = "A"
-#   ttl     = 300
-#   proxied = false
-# }
+resource "cloudflare_dns_record" "k8s_worker_ipv4" {
+  zone_id = local.current_cf_zone_id
+  count   = length(hcloud_server.k8s_worker)
+  name    = hcloud_server.k8s_worker[count.index].name
+  content = hcloud_server.k8s_worker[count.index].ipv4_address
+  type    = "A"
+  ttl     = 300
+  proxied = false
+}
 
-# resource "cloudflare_dns_record" "k8s_worker_ipv6" {
-#   zone_id = local.current_cf_zone_id
-#   count   = length(hcloud_server.k8s_worker)
-#   name    = hcloud_server.k8s_worker[count.index].name
-#   content = hcloud_server.k8s_worker[count.index].ipv6_address
-#   type    = "AAAA"
-#   ttl     = 300
-#   proxied = false
-# }
+resource "cloudflare_dns_record" "k8s_worker_ipv6" {
+  zone_id = local.current_cf_zone_id
+  count   = length(hcloud_server.k8s_worker)
+  name    = hcloud_server.k8s_worker[count.index].name
+  content = hcloud_server.k8s_worker[count.index].ipv6_address
+  type    = "AAAA"
+  ttl     = 300
+  proxied = false
+}
